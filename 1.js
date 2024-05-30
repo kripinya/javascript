8a377219c6e18eea105b6b5f1fdef46ff3e61a7e
kripinya <146142627+kripinya@users.noreply.github.com>
2024-05-30 20:04:28 +0530

    1.js

--- 1.js
+++ 1.js
@@ -2,13 +2,13 @@ console.log("abracadabra");
 let a = 5;
 let b = 4;
 console.log("a = ", a, "& b = ", b);
-console.log("a + b = ", a+b);
-console.log("a - b = ", a-b);
-console.log("a * b = ", a*b);
-console.log("a / b = ", a/b);
+console.log("a + b = ", a+b);//addition
+console.log("a - b = ", a-b);//subtraction
+console.log("a * b = ", a*b);//multiplication
+console.log("a / b = ", a/b);//division
 //modulus
-console.log("a % b= ", a % b);
-console.log("a++", a++);
-console.log("a--", --a);
-console.log("++a = ", ++a);
+console.log("a % b= ", a % b);//modulus
+console.log("a++", a++);//increment operator
+console.log("a--", --a);//decrement operator
+console.log("++a = ", ++a);//pre-increment operator
 console.log("a += 2 = ", a+=2);
